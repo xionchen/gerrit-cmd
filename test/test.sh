76e4ff2ab43b1263f0990d0bfc5afbd38d0e1625
Xiang Chen <xionchen@foxmail.com>
2016-09-08 17:04:06 +0800

    Add some test

--- test/test.sh
+++ test/test.sh
@@ -1,1 +1,7 @@
-PYTHONPATH=`pwd`/../gerrit_cmd/ python `pwd`/../gerrit_cmd/client/shell/shell.py project list -n 100 --substring neutron
+set -x
+gerrit_cmd project get
+gerrit_cmd project list -n 5
+gerrit_cmd project list --substring nova -n 10
+gerrit_cmd project get --name openstack/nova
+gerrit_cmd project create
+gerrit_cmd project create --name test/test
